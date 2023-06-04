a3769913d9c4ad153cb59083bb5dffee019cbf58
Drew Ridley <drew.ridley03@gmail.com>
2023-06-03 20:41:48 -0400

    M1 mac success

--- build.rs
+++ build.rs
@@ -1,9 +1,23 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
+use walkdir::WalkDir;
+
+
+fn find_lib(filename: &str) -> Option<String> {
+
+    let user_dir = directories::UserDirs::new().expect("Home directories should exist!");
+
+    let path = user_dir.home_dir();
+    for entry in WalkDir::new(path).follow_links(true).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_name() == filename {
+            return Some(entry.path().display().to_string());
+        }
+    }
+    None
+}
 
 fn main() {
     // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=/fake/path/to/nonexistent/to/lib");
 
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
@@ -12,10 +26,61 @@ fn main() {
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
+
+
+    //  let lib_dir = PathBuf::from(
+    //      "/Volumes/T7/Projects/scratchpad/Abomination/AbominationInterop/AbominationInterop/bin/Release/net8.0/osx.13-arm64/publish"
+    //  );
+
+    // println!("cargo:rustc-link-search={}", lib_dir.to_str().unwrap()); 
+
+    //println!("cargo:rustc-link-lib=dylib=AbominationInterop");
+
+    let required_libs = [
+        //Generated with:
+        // https://github.com/dotnet/runtime/issues/70277
+        //"sfx_combined.a"
+
+        "libbootstrapperdll.a",
+        "libRuntime.WorkstationGC.a",
+        "libeventpipe-disabled.a",
+        "libstdc++compat.a",
+        "libSystem.Native.a",
+        "libSystem.IO.Compression.Native.a",
+        "libSystem.Net.Security.Native.a",
+        "libSystem.Security.Cryptography.Native.Apple.a",
+        "libSystem.Security.Cryptography.Native.OpenSsl.a",
+        "libSystem.Globalization.Native.a"
+    ];
+
+    for lib_name in required_libs {
+        let lib_path = find_lib(lib_name).expect(&format!("Could not find required library: {}", lib_name));
+        println!("cargo:rustc-link-lib=static:+verbatim={}", lib_path);
+    }
+
+    println!("cargo:rustc-link-lib=static:+verbatim=/Volumes/T7/Projects/scratchpad/Abomination/AbominationInterop/AbominationInterop/bin/Release/net8.0/osx.13-arm64/publish/AbominationInterop.a");
+        
+    
+    //Required for M1 Mac
+    println!("cargo:rustc-link-lib=objc");
+    println!("cargo:rustc-link-lib=swiftCore");
+    println!("cargo:rustc-link-lib=swiftFoundation");
+    println!("cargo:rustc-link-lib=icucore");
+    
+    println!("cargo:rustc-link-arg=_NativeAOT_StaticInitialization");
+
+    println!("cargo:rustc-link-search=/usr/lib/swift");
+
+
     let bindings = bindgen::Builder::default()
 
         .clang_args(&["-x", "c++"])
         .clang_args(&["-std=c++14"])
+
+        //Required for MacOS apparently...
+        .clang_args(&["-framework", "Foundation"])
+        .clang_args(&["-framework", "Security"])
+        .clang_args(&["-framework", "GSS"])
 
         // The input header we would like to generate
         // bindings for.
